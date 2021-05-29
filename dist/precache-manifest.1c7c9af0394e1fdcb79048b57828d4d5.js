48ea994031b6cce487b84938c1d784ef5fc29966
James Devon <jamesdevon@protonmail.com>
2021-05-29 15:44:04 +0100

    adding dist subtree

---
+++ dist/precache-manifest.1c7c9af0394e1fdcb79048b57828d4d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1753fbf93ca186b25bf0",
+    "url": "/deltavmap/css/app.b846bb33.css"
+  },
+  {
+    "revision": "0c5b99827f737ad076ca",
+    "url": "/deltavmap/css/chunk-vendors.6b4fd6b3.css"
+  },
+  {
+    "revision": "9e2d8343e0b537a63fdaf31ff4e2457c",
+    "url": "/deltavmap/index.html"
+  },
+  {
+    "revision": "1753fbf93ca186b25bf0",
+    "url": "/deltavmap/js/app.695093f5.js"
+  },
+  {
+    "revision": "0c5b99827f737ad076ca",
+    "url": "/deltavmap/js/chunk-vendors.045c4f26.js"
+  },
+  {
+    "revision": "61ed7445b726b2381b401adad64044ef",
+    "url": "/deltavmap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deltavmap/robots.txt"
+  }
+]);

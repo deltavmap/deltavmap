7e7ac326bf2068df844887cc8ad36167caaa4118
James Devon <jamesdevon@protonmail.com>
2021-05-29 12:24:40 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <DeltaVMap></DeltaVMap>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DeltaVMap from './components/DeltaVMap'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DeltaVMap
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

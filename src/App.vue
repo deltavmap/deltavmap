822e11faebd34fbbe107690d6795b87d60d46969
James Devon <jamesdevon@protonmail.com>
2021-06-03 09:18:15 +0100

    add redirect to deltavmap.github.io

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <v-app>
-    <DeltaVMap></DeltaVMap>
+    <v-container >
+      <div class="display-1 mt-12" style="margin: 0 auto; max-width: 600px">
+        <p>This site is now hosted at <a href="https://deltavmap.github.io">deltavmap.github.io</a></p>
+        <br>
+        <p>You will be redirected there shortly.</p>
+      </div>
+    </v-container>
   </v-app>
 </template>
 <script>
-import DeltaVMap from './components/DeltaVMap'
 
 export default {
-  name: 'App',
-  components: {
-    DeltaVMap
-  }
+  name: 'App'
 }
 </script>
